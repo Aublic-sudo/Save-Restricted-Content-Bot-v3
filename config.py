8a3fdc6e2a020c615785b4fdce29a559fd2fa004
Aublic-sudo <rstock937@gmail.com>
2025-06-02 00:24:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+
 # Copyright (c) 2025 devgagan : https://github.com/devgaganin.  
 # Licensed under the GNU General Public License v3.0.  
 # See LICENSE file in the repository root for full license text.
@@ -24,14 +25,13 @@ MONGO_DB = os.getenv("MONGO_DB", "")
 OWNER_ID = list(map(int, os.getenv("OWNER_ID", "").split())) # list seperated via space
 DB_NAME = os.getenv("DB_NAME", "telegram_downloader")
 STRING = os.getenv("STRING", None) # optional
-LOG_GROUP = int(os.getenv("LOG_GROUP", "-1001234456")) # optional with -100
-FORCE_SUB = int(os.getenv("FORCE_SUB", "-10012345567")) # optional with -100
-MASTER_KEY = os.getenv("MASTER_KEY", "gK8HzLfT9QpViJcYeB5wRa3DmN7P2xUq") # for session encryption
-IV_KEY = os.getenv("IV_KEY", "s7Yx5CpVmE3F") # for decryption
+LOG_GROUP = int(os.getenv("LOG_GROUP", "")) # optional with -100
+FORCE_SUB = int(os.getenv("FORCE_SUB", "-1002620232917")) # optional with -100
+MASTER_KEY = os.getenv("MASTER_KEY", "gK8HzLfT9QpViJcYeB5wRa3D45mN7P2xUq") # for session encryption
+IV_KEY = os.getenv("IV_KEY", "s7Yx5CpV45mE3F") # for decryption
 YT_COOKIES = os.getenv("YT_COOKIES", YTUB_COOKIES)
 INSTA_COOKIES = os.getenv("INSTA_COOKIES", INST_COOKIES)
-FREEMIUM_LIMIT = int(os.getenv("FREEMIUM_LIMIT", "0"))
-PREMIUM_LIMIT = int(os.getenv("PREMIUM_LIMIT", "500"))
-JOIN_LINK = os.getenv("JOIN_LINK", "https://t.me/team_spy_pro") # this link for start command message
-ADMIN_CONTACT = os.getenv("ADMIN_CONTACT", "https://t.me/username_of_admin")
-
+FREEMIUM_LIMIT = int(os.getenv("FREEMIUM_LIMIT", ""))
+PREMIUM_LIMIT = int(os.getenv("PREMIUM_LIMIT", ""))
+JOIN_LINK = os.getenv("JOIN_LINK", "https://t.me/https://t.me/Gesfdfdfdfds") # this link for start command message#
+ADMIN_CONTACT = os.getenv("ADMIN_CONTACT", "https://t.me/Aublic")
