25e50cebcbffcb77a03169f22648031240d7b355
Aublic-sudo <rstock937@gmail.com>
2025-06-02 12:01:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -33,5 +33,5 @@ YT_COOKIES = os.getenv("YT_COOKIES", YTUB_COOKIES)
 INSTA_COOKIES = os.getenv("INSTA_COOKIES", INST_COOKIES)
 FREEMIUM_LIMIT = int(os.getenv("FREEMIUM_LIMIT", "0"))
 PREMIUM_LIMIT = int(os.getenv("PREMIUM_LIMIT", "99999999"))
-JOIN_LINK = os.getenv("JOIN_LINK", "https://t.me/Gesfdfdfdfds") # this link for start command message#
+JOIN_LINK = os.getenv("JOIN_LINK", "https://t.me/elmlQ7Uxt84MzE1") # this link for start command message#
 ADMIN_CONTACT = os.getenv("ADMIN_CONTACT", "https://t.me/Aublic")
