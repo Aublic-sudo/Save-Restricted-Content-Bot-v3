c6ba907f636cc8b6ce525876bb64d3493c5a6632
Aublic-sudo <rstock937@gmail.com>
2025-06-02 08:20:07 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -153,42 +153,7 @@ async def on_help_navigation(client, callback_query):
     await callback_query.answer()
 
  
-@app.on_message(filters.command("terms") & filters.private)
-async def terms(client, message):
-    terms_text = (
-        "> 📜 **Terms and Conditions** 📜\n\n"
-        "✨ We are not responsible for user deeds, and we do not promote copyrighted content. If any user engages in such activities, it is solely their responsibility.\n"
-        "✨ Upon purchase, we do not guarantee the uptime, downtime, or the validity of the plan. __Authorization and banning of users are at our discretion; we reserve the right to ban or authorize users at any time.__\n"
-        "✨ Payment to us **__does not guarantee__** authorization for the /batch command. All decisions regarding authorization are made at our discretion and mood.\n"
-    )
-     
-    buttons = InlineKeyboardMarkup(
-        [
-            [InlineKeyboardButton("📋 See Plans", callback_data="see_plan")],
-            [InlineKeyboardButton("💬 Contact Now", url="https://t.me/Aublic")],
-        ]
-    )
-    await message.reply_text(terms_text, reply_markup=buttons)
- 
- 
-@app.on_message(filters.command("plan") & filters.private)
-async def plan(client, message):
-    plan_text = (
-        "> 💰 **Premium Price**:\n\n Starting from $2 or 200 INR accepted via **__Amazon Gift Card__** (terms and conditions apply).\n"
-        "📥 **Download Limit**: Users can download up to 100,000 files in a single batch command.\n"
-        "🛑 **Batch**: You will get two modes /bulk and /batch.\n"
-        "   - Users are advised to wait for the process to automatically cancel before proceeding with any downloads or uploads.\n\n"
-        "📜 **Terms and Conditions**: For further details and complete terms and conditions, please send /terms.\n"
-    )
-     
-    buttons = InlineKeyboardMarkup(
-        [
-            [InlineKeyboardButton("📜 See Terms", callback_data="see_terms")],
-            [InlineKeyboardButton("💬 Contact Now", url="https://t.me/Aublic")],
-        ]
-    )
-    await message.reply_text(plan_text, reply_markup=buttons)
- 
+
  
 
  
