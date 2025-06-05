64f5019eb5d67075929f0a0028d3405f1da589d1
Aublic-sudo <rstock937@gmail.com>
2025-06-05 19:59:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 # Copyright (c) 2025 devgagan : https://github.com/devgaganin.  
 # Licensed under the GNU General Public License v3.0.  
 # See LICENSE file in the repository root for full license text.
@@ -38,4 +39,4 @@ if __name__ == "__main__":
         try:
             loop.close()
         except Exception:
-            pass 
+            pass    
