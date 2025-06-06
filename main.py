65d58e8b48c8a3ac2e77594e3c1ea9314037aa22
Aublic-sudo <rstock937@gmail.com>
2025-06-06 16:51:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 
+
 # Copyright (c) 2025 devgagan : https://github.com/devgaganin.  
 # Licensed under the GNU General Public License v3.0.  
 # See LICENSE file in the repository root for full license text.
@@ -18,19 +19,25 @@ async def load_and_run_plugins():
         module = importlib.import_module(f"plugins.{plugin}")
         if hasattr(module, f"run_{plugin}_plugin"):
             print(f"Running {plugin} plugin...")
-            await getattr(module, f"run_{plugin}_plugin")()
+            await getattr(module, f"run_{plugin}_plugin")()  
 
 async def main():
     await load_and_run_plugins()
     while True:
-        await asyncio.sleep(1)
+        await asyncio.sleep(1)  
 
 if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
     print("Starting clients ...")
     try:
-        asyncio.run(main())
+        loop.run_until_complete(main())
     except KeyboardInterrupt:
         print("Shutting down...")
     except Exception as e:
         print(e)
         sys.exit(1)
+    finally:
+        try:
+            loop.close()
+        except Exception:
+            pass    
